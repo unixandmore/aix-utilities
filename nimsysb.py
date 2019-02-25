f9c49980994b6d3727d222e75aee1bf06ba6367c
Raymond Cox <rcox@unixandmore.com>
2019-02-25 09:36:44 -0600

    Added some command line switches and a new routine to list mksysbs

--- nimsysb.py
+++ nimsysb.py
@@ -26,11 +26,11 @@ class NimSYSB(object):
     def __init__(self, **kwargs):
         """Prepare to make a connection to the remote host
         Args:
-            host (str): Hostname or IP address of the remote AIX system, if hostname is used it must be in DNS or /etc/hosts
-            username (str): Defaults to root
-            sshkey (str): Full path to the key being used to allow remote access to the AIX system
+            client (str): The NIM client we are creating the mksysb for
+            nas_filer (str): The nas_filer object created to store the images.
+            vfs (str): Virtual file system on the nas_filer.
         """
-        allowed_params = ['nas_filer', 'nas_filer_dir' ]
+        allowed_params = ['nas_filer', 'nas_filer_dir', 'client' ]
         params = locals()
         for key, val in params['kwargs'].items():
             if key not in allowed_params:
@@ -50,6 +50,10 @@ class NimSYSB(object):
         else:
             self.nas_filer_dir = params['nas_filer_dir']
 
+        if ('client' not in params) or (params['client'] is None):
+            raise ValueError("Missing the required parameter `client` when calling `__init__`")
+        else:
+            self.client = params['client']
 
         self.logger = logging.getLogger(__name__)
         self.logger.addHandler(logging.NullHandler())
@@ -61,36 +65,46 @@ class NimSYSB(object):
 
     def list_systems(self):
         """Helper function to list the defined standalone systems in NIM"""
+        nim_clients = []
         cmd = 'lsnim -t standalone'
-        process = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        while True:
-            output = process.stdout.readline().decode('utf-8').strip()
-            if output:
-                print('{}'.format(output.strip()))
+        for line in subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').split('\n'):
+            line = line.strip().split(' ')
+            nim_clients.append(line[0])
+        return nim_clients
+
+    def list_mksysb(self):
+        """Helper function to list mksysb objects"""
+        
+
     
-    def getargs():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-c','--client',
-                            action='store',
-                            required=True,
-                            dest='client',
-                            help='Client name as stored on the NIM server')
-        parser.add_argument('-n','--nas',
-                            action='store',
-                            required=True,
-                            dest='server',
-                            help='Specify the name of the nas_filer object in the NIM server, or master for other operations')
-        parser.add_argument('--vfs',
-                            action='store',
-                            required=True,
-                            dest='vfs',
-                            help='The Virtual File System path on the NAS device')
-        args = parser.parse_args()
-        return args
+def getargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c','--client',
+                        action='store',
+                        required=True,
+                        dest='client',
+                        help='Client name as stored on the NIM server')
+    parser.add_argument('-n','--nas',
+                        action='store',
+                        required=True,
+                        dest='nas_filer',
+                        help='Specify the name of the nas_filer object in the NIM server, or master for other operations')
+    parser.add_argument('--vfs',
+                        action='store',
+                        required=True,
+                        dest='vfs',
+                        help='The Virtual File System path on the NAS device')
+    args = parser.parse_args()
+    return args
 
 
-nimsysb = NimSYSB(nas_filer='netapp_8040', nas_filer_dir='/vol_advnim_mksysb')
-nimsysb.list_systems()
+#nimsysb = NimSYSB(nas_filer='netapp_8040', nas_filer_dir='/vol_advnim_mksysb')
+#nimsysb.list_systems()
 if __name__ == '__main__':
     args = getargs()
+    NB = NimSYSB(nas_filer=args.nas_filer,nas_filer_dir=args.vfs,client=args.client)
+    nim_clients = NB.list_systems()
+    if nim_clients:
+        pprint(nim_clients, depth=4)
+
 
